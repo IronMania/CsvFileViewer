c363bf1265ca6516cb242d428695eb437b4c21f6
Jörg Weissbecker <jw@co-IT.eu>
2018-07-07 15:12:26 +0200

    Add project files.

---
+++ CsvFileViewer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsvFileViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
